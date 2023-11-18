ca7744cd906353c498e8c1418424c6c0d84a01e4
AlisArch <alfrednguyen02@gmail.com>
2023-11-18 21:25:27 +0100

    get requests working

--- app/pages/get_request.py
+++ app/pages/get_request.py
@@ -1,8 +1,11 @@
-import streamlit as st
-import os
-import requests
-
 from dotenv import load_dotenv
+import os
+import streamlit as st
+import requests
+import urllib
+from tqdm import tqdm  # Added import for tqdm
+import urllib.request
+
 
 def read_env_file():
     load_dotenv()
@@ -18,12 +21,28 @@ def make_api_request(url, headers):
     except requests.exceptions.RequestException as err:
         return f"Request Error: {err}"
 
+def download_video(url, save_path):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+        print("Download complete.")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+
+
+
+
+
 # Streamlit app
 st.title("SyncLabs API Request Example")
+pwd = os.getcwd()
+st.write(pwd)
 
 # Input fields
 url = st.text_input("Enter API URL:", 'https://api.synclabs.so/video/f38c6133-0c9a-4857-ab0a-dc1c4663cb45')
-
 api_key = read_env_file()
 
 # Make the API request when the user clicks the button
@@ -38,6 +57,19 @@ if st.button("Make API Request"):
     # Display the result
     st.header("API Response:")
     st.json(result)
+
+    # Download video button
+    if 'url' in result:
+        video_url = result['url']
+        st.write(video_url)
+        download_video(video_url, "data/download.mp4")
+    
+    st.write("Your video")
+    # File path to your MP4 video
+    video_path = "data/download.mp4"
+
+    # Display the video
+    st.video(video_path)
 
 
 
